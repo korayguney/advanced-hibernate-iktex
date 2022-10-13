7559ca8515d24b4466b72ce764e1a936bf4e03e2
Koray Guney <koray.guney@hotmail.com>
2022-10-13 14:24:23 +0300

    added spring boot module

---
+++ hb-04/src/main/java/com/iktex/hb04/Hb04Application.java
@@ -0,0 +1,13 @@
+package com.iktex.hb04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hb04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hb04Application.class, args);
+    }
+
+}
---
+++ hb-04/src/test/java/com/iktex/hb04/Hb04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktex.hb04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hb04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
